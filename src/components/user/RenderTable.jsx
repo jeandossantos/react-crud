223fb043a76f12dbd52f9887170144b997fbb675
txtdbr <jeanddg@hotmail.com>
2019-10-01 18:12:26 -0300

    rendertable

---
+++ src/components/user/RenderTable.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import UserCrud from './UserCrud';
+
+export default class RenderTable extends React.Component {
+    
+
+    render(){
+        return (
+            <React.Fragment>
+                {this.renderTable()}
+            </React.Fragment>
+        )
+    }
+    renderTable() {
+        console.log(this.props)
+        return (
+            <table className="table mt-4">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderRows()}
+                </tbody>
+            </table>
+        )
+    }
+
+    renderRows() {
+        return this.props.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn btn-warning"
+                            onClick={() => UserCrud.load(user)}>
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn btn-danger ml-2"
+                            onClick={() => this.remove(user)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+}
